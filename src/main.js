97d5ff39e0c8d447ce85a5d3633339d6470fdf3e
Danny Chris <dannychrisa1.githu.io>
2022-03-05 15:17:57 +0100

    added modalform

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App.vue'
 import './assets/global.css'
 import router from './router'
